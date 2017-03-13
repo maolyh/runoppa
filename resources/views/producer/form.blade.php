8eb85e7194635d4119bd3cd133b0af10780d3080
maolyh <maolyh@gmail.com>
2017-03-13 14:29:30 -0400

    acordeon en la forma producer

--- resources/views/producer/form.blade.php
+++ resources/views/producer/form.blade.php
@@ -1,31 +1,47 @@
 
 
-<form>
+<form class="form-inline">
 {{csrf_field()}}
 
 <div class="container">
   <div class="row-fluid">
-    <div class="hero-unit span8">
+    <div class="hero-unit span10">
       <div id="valor">
         <div class="row">
           <div class="span3"> 
             <label for="persontype">Tipo de persona</label>
           </div>
-          
+         
           <div class="span1"><input type="radio" id="naturalPersonType" name="PT" value="Persona Natural" v-model="picked" v-on:change="greet"> </div>
           <div class="span2"><label for="naturalPersonType">Natural</label></div>
           <div class="span1"><input type="radio" id="legalPersonType" name="PT" value="Persona Jurídica" v-model="picked" v-on:change="greet" ></div>
           <div class="span2"><label for="legalPersonType">Jurídica</label></div>
-          
         </div>
+        
+        <hr>
         <div class="row">
-          <div class="span12">
-           <label for="personTypeSelected"> @{{picked}} </label>
+          <div class="span12 panel-title">
+           <label for="personTypeSelected"><h3> @{{picked}}</h3> </label>
           </div>
         </div>  
-
-        <!--Persona Natural-->
-        <div id="block" v-if="block">
+      <hr>
+       <div id="block" v-if="block">
+      <!-- Inicio del acordeon-->
+      <div class="row">
+          <div class="span12">
+ <div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+        Información Personal</a>
+      </h4>
+    </div>
+    <div id="collapse1" class="panel-collapse collapse in">
+      <div class="panel-body">
+        
+        
+                
           <div class="row"> 
             <div class="span2">
               <label for="preidentificator">@{{preidentificator}} </label>
@@ -115,11 +131,9 @@
                   <input type="text" class="form-control" id="instructionlevel" name="instructionlevel" placeholder="Nivel de instrucción">
               </div>
             </div>
-          </div>      
-    <!--End Natural Person-->
-     
-    <!--Legal Person-->
-        <div id="legalPerson" v-if="legalBlock">
+          </div> 
+
+ <div id="legalPerson" v-if="legalBlock">
           <div class="row">
             <div class="span2">
               <label for="socialname">Razón social</label>
@@ -128,10 +142,24 @@
                 <input type="text" class="form-control" id="socialname" name="socialname" placeholder="Razón social">
               </div>
             </div>
-         </div>   
-      <!--End Legal Person-->
+         </div>             
+           
 
-          <div class="row">
+
+        
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+        Datos de contacto</a>
+      </h4>
+    </div>
+    <div id="collapse2" class="panel-collapse collapse">
+      <div class="panel-body">
+ <div class="row">
             <div class="span2">
               <label for="homephone">Teléfono local</label>
             </div>
@@ -144,10 +172,8 @@
             <div class="span4">
                 <input type="text" class="form-control" id="celphone" name="celphone" placeholder="Teléfono celular">
             </div>
-          </div>
-
-      <!--Legal Person-->
-      <div id="legalPerson" v-if="legalBlock">
+          </div>  
+                 <div id="legalPerson" v-if="legalBlock">
           <div class="row">
             <div class="span2">
               <label for="homephone2">Teléfono local 2</label>
@@ -173,7 +199,8 @@
               <input type="email" class="form-control" id="email" name="email" placeholder="Email">
             </div>
           </div>
-
+          <hr>
+          <div class="divider"></div>
           <div class="row">
             <div class="span2">
               <label for="state">Estado</label>
@@ -215,7 +242,44 @@
              </textarea>
             </div>
           </div>
-         </div> 
+     
+
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+        Información de la producción</a>
+      </h4>
+    </div>
+    <div id="collapse3" class="panel-collapse collapse">
+      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+
+     
+        <!--Persona Natural-->
+       
+    <!--End Natural Person-->
+     
+    <!--Legal Person-->
+       
+      <!--End Legal Person-->
+          <hr>
+          
+
+      <!--Legal Person-->
+         </div>
+       
       <div class="form-group">
         <div class="span12">
           <button type="submit" class="btn btn-primary">Registrar</button>
